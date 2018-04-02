aabeb47f5a6dc42971a8094bb936d46de904b067
Trojaner_ <trojaner25@gmail.com>
2018-04-02 14:53:26 +0300

    Initialize Universal Template

---
+++ Rocket.Templates.Universal/PluginMain.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Rocket.API.DependencyInjection;
+using Rocket.API.Logging;
+using Rocket.Core.Plugins;
+
+namespace $rootnamespace$
+{
+    public class $safeprojectname$Plugin : PluginBase
+    {
+        private readonly ILogger _logger;
+
+        public PluginMain(IDependencyContainer container, ILogger logger) : base(container)
+        {
+            _logger = logger;
+        }
+
+        public override void Load()
+        {
+            _logger.Info("Hello world!");
+        }
+
+        public override void Unload()
+        {
+            
+        }
+
+        public override IEnumerable<string> Capabilities => new List<string>();
+
+        public override string Name => "$safeprojectname$";
+    }
+}
